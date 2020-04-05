5847771717950fb2f7afbb24eae352437741faab
nikhil <rocknikhil1908@gmail.com>
2020-04-05 23:00:58 +0530

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Home1Component } from './home1/home1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    Home1Component
   ],
   imports: [
     BrowserModule,
