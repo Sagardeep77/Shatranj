7d21b0e77555dd0265c4e9b725b166a27794baad
Sagar deep kushwaha <25485584+Sagardeep77@users.noreply.github.com>
2024-03-12 23:46:41 +0530

    backend url

--- src/app/services/data-communication.service.ts
+++ src/app/services/data-communication.service.ts
@@ -9,14 +9,15 @@ export class DataCommunicationService {
   canUseCode:boolean=false;
   constructor(private httpClient:HttpClient) { }
 
-  checkCodeUrl="http://localhost:4000?code=";
-  webSocketUrl="http://localhost:4000"
+  checkCodeUrl="https://shatranj-backend-9wbyg500w-sagardeep77.vercel.app?code=";
+  webSocketUrl="https://shatranj-backend-9wbyg500w-sagardeep77.vercel.app"
   
 
   async checkCode(code):Promise<boolean>{
     const response = await this.httpClient.get(this.checkCodeUrl+code).toPromise().then((response)=>{
         let stringifiedResponse = JSON.stringify(response);
         this.canUseCode= <boolean>JSON.parse(stringifiedResponse).canUse;
+        // console.log("canUSeCode-->",this.canUseCode);
     });
     return this.canUseCode;
   }
